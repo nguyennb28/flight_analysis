6146ae9321a9c9fc80f5974a56f53ea6ea962d7c
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-05 08:46:33 +0700

    setup User Model

--- backend/data_analysis/models.py
+++ backend/data_analysis/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [("admin", "Admin"), ("user", "User")]
+    phone = models.CharField(max_length=10, blank=True, null=True)
+    role = models.CharField(
+        max_length=50, choices=ROLE_CHOICES, default="user", verbose_name="Role"
+    )
+
+    def __str__(self):
+        return self.get_full_name()
---
+++ backend/data_analysis/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+# router.register(r"users", views.UserViewSet)
+# router.register(r"ports", views.PortViewSet)
+# router.register(r"container-sizes", views.ContainerSizeViewSet)
+# router.register(r"vat-infos", views.VatInfoViewSet)
+# router.register(r"agencies", views.AgencyViewSet)
+# router.register(r"cfss", views.CFSViewSet)
+# router.register(
+#     "databases", views.DatabaseViewSet, basename="database"
+# )  # Phải chỉ định basename
+# router.register(r"door", views.DoorViewSet)
+# router.register(r"door-fee-detail", views.DoorFeeViewSet)
+# router.register(r"door-delivery-fee", views.DoorDeliveryFeeViewSet)
+# router.register(r"door-container", views.DoorContainerViewSet)
+urlpatterns = [path("", include(router.urls))]
