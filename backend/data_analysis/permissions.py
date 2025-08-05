9eec4f75a520ff3f0b7296130a3fd93b920c1641
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-05 09:11:32 +0700

    complete make permissions

---
+++ backend/data_analysis/permissions.py
@@ -0,0 +1,28 @@
+from rest_framework import permissions
+
+
+class IsRoleAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.role == "admin"
+        )
+
+
+class IsRoleUser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user
+            and request.user.is_authenticated
+            and request.user.role == "user"
+        )
+
+
+class IsBothOfUs(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user
+            and request.user.authenticated
+            and request.user.role in ["admin", "user"]
+        )
