3448ee46de51e93eb2dfdb3bf59e8a9a12227a25
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-05 22:11:36 +0700

    make udpate for user

--- dashboard/src/pages/UserProfiles.tsx
+++ dashboard/src/pages/UserProfiles.tsx
@@ -5,11 +5,39 @@ import PageMeta from "../components/common/PageMeta";
 import { useAuth } from "../context/AuthContext";
 import { useEffect } from "react";
 import { useLoading } from "../context/LoadingContext";
+import Swal from "sweetalert2";
+import { User } from "../types/general_type";
+import axiosInstance from "../instance/axiosInstance";
 
 export default function UserProfiles() {
   // Context
   const { user } = useAuth();
   const { loading, showLoading, hideLoading } = useLoading();
+
+  const onSubmit = async (data: User) => {
+    if (!data) {
+      Swal.fire({
+        icon: "error",
+        title: "Dữ liệu rỗng",
+        text: "Cung cấp dữ liệu",
+      });
+      return;
+    }
+    try {
+      // const response = await axiosInstance.put(`/api/users/${data.id}`, {
+      //   ...data
+      // })
+      console.log(data);
+      // console.log({ ...data });
+    } catch (err: any) {
+      console.error(err);
+      Swal.fire({
+        icon: "error",
+        title: "Lỗi trong quá trình cập nhật thông tin người dùng",
+        text: "Có lỗi trong việc cập nhật thông tin người dùng",
+      });
+    }
+  };
 
   useEffect(() => {
     if (!user) {
@@ -25,10 +53,7 @@ export default function UserProfiles() {
 
   return (
     <>
-      <PageMeta
-        title="Profile"
-        description="Profile"
-      />
+      <PageMeta title="Profile" description="Profile" />
       <PageBreadcrumb pageTitle="Profile" />
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] lg:p-6">
         <h3 className="mb-5 text-lg font-semibold text-gray-800 dark:text-white/90 lg:mb-7">
@@ -52,6 +77,7 @@ export default function UserProfiles() {
                 full_name={user.full_name}
                 role={user.role}
                 phone={user.phone}
+                onSubmit={onSubmit}
               />
             </>
           )}
---
+++ dashboard/src/types/general_type.ts
@@ -0,0 +1,7 @@
+export type User = {
+  id?: number;
+  first_name: string | null;
+  last_name: string | null;
+  role: string;
+  phone: string | null;
+};
