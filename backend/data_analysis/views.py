8c99573157d84318b1bdac0f66d343e0954de99e
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-11 14:30:21 +0700

    refactor raw_sheet

--- backend/data_analysis/views.py
+++ backend/data_analysis/views.py
@@ -54,6 +54,106 @@ class UserViewSet(viewsets.ModelViewSet):
 class UploadExcel(APIView):
     parser_classes = (MultiPartParser, FormParser)
 
+    SHEET_NAMES = ["Chuyenbay", "Thongtinchung", "Hanhkhach", "PNR"]
+    LABEL_NAMES = [
+        {
+            "sheet": "Chuyenbay",
+            "headers": ["Thông tin chuyến bay"],
+            "first_row": [
+                "Hãng vận chuyển",
+                "Nhãn hiệu quốc tịch",
+                "Số chuyến bay",
+                "Ngày bay",
+                "Nơi đi",
+                "Nơi đến",
+                "Đường bay",
+                "Nơi quá cảnh",
+            ],
+            "second_row": None,
+            "first_skiprow": 2,
+            "second_skiprow": None,
+            "first_nrows": None,
+            "second_nrows": None,
+        },
+        {
+            "sheet": "Thongtinchung",
+            "headers": ["Thông tin chung", "Danh sách thành viên tổ bay"],
+            "first_row": [
+                "Số khách",
+                "Nơi đi",
+                "Nơi xuất cảnh",
+                "Through on same flight",
+                "Nơi đến",
+                "Nơi nhập cảnh",
+                "Through on same flight",
+            ],
+            "second_row": [
+                "Họ và tên",
+                "Giới tính",
+                "Quốc tịch",
+                "Ngày sinh",
+                "Số giấy tờ",
+                "Loại giấy tờ",
+                "Nơi cấp",
+                "Ngày hết hạn",
+            ],
+            "first_skiprow": 2,
+            "second_skiprow": 7,
+            "first_nrows": 1,
+            "second_nrows": None,
+        },
+        {
+            "sheet": "Hanhkhach",
+            "headers": ["Danh sách hành khách"],
+            "first_row": [
+                "Số ghế",
+                "Họ và tên",
+                "Giới tính",
+                "Quốc tịch",
+                "Ngày sinh",
+                "Loại giấy tờ",
+                "Số giấy tờ",
+                "Nơi cấp",
+                "Quốc gia cư trú",
+                "Nơi đi",
+                "Nơi đến",
+                "Cảng hàng không đầu tiên",
+                "Hành lý",
+                "Ngày hết hạn",
+            ],
+            "second_row": None,
+            "first_skiprow": 2,
+            "second_skiprow": None,
+            "first_nrows": None,
+            "second_nrows": None,
+        },
+        {
+            "sheet": "PNR",
+            "headers": ["Danh sách hành khách PNR"],
+            "first_row": [
+                "Mã đặt chỗ",
+                "Ngày đặt chỗ",
+                "Thông tin vé",
+                "Tên hành khách",
+                "Tên khác",
+                "Hành trình bay",
+                "Địa chỉ",
+                "Điện thoại/Email",
+                "Thông tin liên hệ",
+                "Số lượng hành khách chung mã đặt chỗ",
+                "Mã người đặt chỗ",
+                "Số ghế",
+                "Thông tin hành lý",
+                "Ghi chú",
+            ],
+            "second_row": None,
+            "first_skiprow": 2,
+            "second_skiprow": None,
+            "first_nrows": None,
+            "second_nrows": None,
+        },
+    ]
+
     def post(self, request, *args, **kwargs):
         uploaded_files = request.FILES.getlist("files")
 
@@ -63,14 +163,11 @@ class UploadExcel(APIView):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        # Ở bước này, chúng ta chỉ cần xác nhận đã nhận được file.
-        # Logic xử lý bằng Pandas sẽ ở bước tiếp theo.
         file_names = [file.name for file in uploaded_files]
 
-        print("Đã nhận được các file:", file_names)  # In ra console để kiểm tra
-
         for excel_file in uploaded_files:
-            self.raw_sheet(excel_file)
+            for sheet_name in self.SHEET_NAMES:
+                self.raw_sheet(excel_file, sheet_name)
 
         return Response(
             {
@@ -79,150 +176,50 @@ class UploadExcel(APIView):
             status=status.HTTP_201_CREATED,
         )
 
-    def raw_sheet(self, file):
-        SHEET_NAMES = ["Chuyenbay", "Thongtinchung", "Hanhkhach", "PNR"]
-        LABEL_NAMES = [
-            {
-                "sheet": "Chuyenbay",
-                "headers": ["Thông tin chuyến bay"],
-                "first_row": [
-                    "Hãng vận chuyển",
-                    "Nhãn hiệu quốc tịch",
-                    "Số chuyến bay",
-                    "Ngày bay",
-                    "Nơi đi",
-                    "Nơi đến",
-                    "Đường bay",
-                    "Nơi quá cảnh",
-                ],
-                "second_row": None,
-                "first_skiprow": 2,
-                "second_skiprow": None,
-                "first_nrows": None,
-                "second_nrows": None,
-            },
-            {
-                "sheet": "Thongtinchung",
-                "headers": ["Thông tin chung", "Danh sách thành viên tổ bay"],
-                "first_row": [
-                    "Số khách",
-                    "Nơi đi",
-                    "Nơi xuất cảnh",
-                    "Through on same flight",
-                    "Nơi đến",
-                    "Nơi nhập cảnh",
-                    "Through on same flight",
-                ],
-                "second_row": [
-                    "Họ và tên",
-                    "Giới tính",
-                    "Quốc tịch",
-                    "Ngày sinh",
-                    "Số giấy tờ",
-                    "Loại giấy tờ",
-                    "Nơi cấp",
-                    "Ngày hết hạn",
-                ],
-                "first_skiprow": 2,
-                "second_skiprow": 7,
-                "first_nrows": 1,
-                "second_nrows": None,
-            },
-            {
-                "sheet": "Hanhkhach",
-                "headers": ["Danh sách hành khách"],
-                "first_row": [
-                    "Số ghế",
-                    "Họ và tên",
-                    "Giới tính",
-                    "Quốc tịch",
-                    "Ngày sinh",
-                    "Loại giấy tờ",
-                    "Số giấy tờ",
-                    "Nơi cấp",
-                    "Quốc gia cư trú",
-                    "Nơi đi",
-                    "Nơi đến",
-                    "Cảng hàng không đầu tiên",
-                    "Hành lý",
-                    "Ngày hết hạn",
-                ],
-                "second_row": None,
-                "first_skiprow": 2,
-                "second_skiprow": None,
-                "first_nrows": None,
-                "second_nrows": None,
-            },
-            {
-                "sheet": "PNR",
-                "headers": ["Danh sách hành khách PNR"],
-                "first_row": [
-                    "Mã đặt chỗ",
-                    "Ngày đặt chỗ",
-                    "Thông tin vé",
-                    "Tên hành khách",
-                    "Tên khác",
-                    "Hành trình bay",
-                    "Địa chỉ",
-                    "Điện thoại/Email",
-                    "Thông tin liên hệ",
-                    "Số lượng hành khách chung mã đặt chỗ",
-                    "Mã người đặt chỗ",
-                    "Số ghế",
-                    "Thông tin hành lý",
-                    "Ghi chú",
-                ],
-                "second_row": None,
-                "first_skiprow": 2,
-                "second_skiprow": None,
-                "first_nrows": None,
-                "second_nrows": None,
-            },
-        ]
-
-        # sheet = None
+    def raw_sheet(self, file, sheet_name):
+        sheet = None
         first_result = None
         second_result = None
 
-        for sheet_name in SHEET_NAMES:
-            for elem in LABEL_NAMES:
-                for detail in elem:
-                    if sheet_name == elem[detail]:
-                        print(elem["sheet"])
-                        if elem["first_row"]:
-                            if elem["first_nrows"]:
-                                first_result = pd.read_excel(
-                                    file,
-                                    sheet_name=elem["sheet"],
-                                    skiprows=elem["first_skiprow"],
-                                    usecols=elem["first_row"],
-                                    nrows=elem["first_nrows"],
-                                )
-                            else:
-                                first_result = pd.read_excel(
-                                    file,
-                                    sheet_name=elem["sheet"],
-                                    skiprows=elem["first_skiprow"],
-                                    usecols=elem["first_row"],
-                                )
-                        if elem["second_row"]:
-                            if elem["second_nrows"]:
-                                second_result = pd.read_excel(
-                                    file,
-                                    sheet_name=elem["sheet"],
-                                    skiprows=elem["second_skiprow"],
-                                    usecols=elem["second_row"],
-                                    nrows=elem["second_nrows"],
-                                )
-                            else:
-                                second_result = pd.read_excel(
-                                    file,
-                                    sheet_name=elem["sheet"],
-                                    skiprows=elem["second_skiprow"],
-                                    usecols=elem["second_row"],
-                                )
-                        if first_result is not None:
-                            print(f"First result:\n {first_result}")
+        for elem in self.LABEL_NAMES:
+            for detail in elem:
+                if sheet_name == elem[detail]:
+                    sheet = elem
 
-                        if second_result is not None:
-                            print(f"Second result:\n {second_result}")
+        if sheet["first_row"]:
+            if sheet["first_nrows"]:
+                first_result = pd.read_excel(
+                    file,
+                    sheet_name=sheet["sheet"],
+                    skiprows=sheet["first_skiprow"],
+                    usecols=sheet["first_row"],
+                    nrows=sheet["first_nrows"],
+                )
+            else:
+                first_result = pd.read_excel(
+                    file,
+                    sheet_name=sheet["sheet"],
+                    skiprows=sheet["first_skiprow"],
+                    usecols=sheet["first_row"],
+                )
+        if sheet["second_row"]:
+            if sheet["second_nrows"]:
+                second_result = pd.read_excel(
+                    file,
+                    sheet_name=sheet["sheet"],
+                    skiprows=sheet["second_skiprow"],
+                    usecols=sheet["second_row"],
+                    nrows=sheet["second_nrows"],
+                )
+            else:
+                second_result = pd.read_excel(
+                    file,
+                    sheet_name=sheet["sheet"],
+                    skiprows=sheet["second_skiprow"],
+                    usecols=sheet["second_row"],
+                )
+        if first_result is not None:
+            print(first_result)
+
+        if second_result is not None:
+            print(second_result)
