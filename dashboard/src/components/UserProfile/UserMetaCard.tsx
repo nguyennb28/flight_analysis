228ca0ef315e3e41ba15429b9321ef193d33052a
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-29 15:47:50 +0700

    modify UserMetaCard

--- dashboard/src/components/UserProfile/UserMetaCard.tsx
+++ dashboard/src/components/UserProfile/UserMetaCard.tsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { Modal } from "../ui/modal";
-import { useForm, SubmitHandler } from "react-hook-form";
-
 interface Props {
   id: number;
   username: string;
@@ -12,15 +8,6 @@ interface Props {
   phone: string | null;
 }
 
-type Inputs = {
-  username: string;
-  password: string;
-  first_name: string;
-  last_name: string;
-  phone: string;
-  role: string;
-};
-
 const UserMetaCard = ({
   id,
   username,
@@ -30,17 +17,6 @@ const UserMetaCard = ({
   role,
   phone,
 }: Props) => {
-  const [isCreate, setIsCreate] = useState<boolean>(false);
-
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm<Inputs>();
-
-  
-
   return (
     <>
       <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
@@ -63,28 +39,8 @@ const UserMetaCard = ({
               <p className="invisible">{phone}</p>
             </div>
           </div>
-          <div>
-            <button
-              type="button"
-              className="border-1 bg-gray-400 rounded-2xl shadow-2xl p-5"
-              onClick={() => setIsCreate(true)}
-            >
-              Tạo tài khoản
-            </button>
-          </div>
         </div>
       </div>
-      <Modal
-        isOpen={isCreate}
-        onClose={() => setIsCreate(false)}
-        className="max-w-[700px] m-4"
-      >
-        <div className="relative w-full p-4 overflow-y-auto bg-white no-scrollbar rounded-3xl dark:bg-gray-900 lg:p-11">
-          <div>
-            <h4 className="text-2xl uppercase font-bold">Tạo tài khoản </h4>
-          </div>
-        </div>
-      </Modal>
     </>
   );
 };
