4e593c6154e6016d1dd188c1fc969b1ce7aaa79a
nguyennb28 <nguyennb.coding@gmail.com>
2025-08-29 09:05:20 +0700

    remove comment lines

--- dashboard/src/pages/Account/TableAccount.tsx
+++ dashboard/src/pages/Account/TableAccount.tsx
@@ -43,38 +43,6 @@ const TableAccount = ({ headers, users, onDelete, onEdit }: Props) => {
                 {header}
               </TableCell>
             ))}
-            {/* <TableRow>
-              <TableCell
-                isHeader
-                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
-              >
-                User
-              </TableCell>
-              <TableCell
-                isHeader
-                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
-              >
-                Project Name
-              </TableCell>
-              <TableCell
-                isHeader
-                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
-              >
-                Team
-              </TableCell>
-              <TableCell
-                isHeader
-                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
-              >
-                Status
-              </TableCell>
-              <TableCell
-                isHeader
-                className="px-5 py-3 font-medium text-gray-500 text-start text-theme-xs dark:text-gray-400"
-              >
-                Budget
-              </TableCell>
-            </TableRow> */}
           </TableHeader>
 
           {/* Table Body */}
@@ -83,7 +51,9 @@ const TableAccount = ({ headers, users, onDelete, onEdit }: Props) => {
               users.map((record, index) => (
                 <TableRow key={index}>
                   <TableCell className="px-5 py-4 sm:px-6 text-start">
-                    <div className="flex items-center gap-3 dark:text-white">{index}</div>
+                    <div className="flex items-center gap-3 dark:text-white">
+                      {index}
+                    </div>
                   </TableCell>
                   <TableCell className="px-5 py-4 sm:px-6 text-start">
                     <div className="flex items-center gap-3 dark:text-white">
@@ -107,12 +77,17 @@ const TableAccount = ({ headers, users, onDelete, onEdit }: Props) => {
                     </div>
                   </TableCell>
                   <TableCell className="px-5 py-4 sm:px-6 text-start">
-                    <div className="flex items-center gap-3 dark:text-white">{record.role}</div>
+                    <div className="flex items-center gap-3 dark:text-white">
+                      {record.role}
+                    </div>
                   </TableCell>
                   <TableCell className="px-5 py-4 sm:px-6 text-start grid grid-cols-1 md:grid-cols-2">
                     <div>
                       <button>
-                        <MdDelete size={30} className="text-red-800 dark:text-red-400" />
+                        <MdDelete
+                          size={30}
+                          className="text-red-800 dark:text-red-400"
+                        />
                       </button>
                     </div>
                     <div>
@@ -123,68 +98,6 @@ const TableAccount = ({ headers, users, onDelete, onEdit }: Props) => {
                   </TableCell>
                 </TableRow>
               ))}
-            {/* {tableData.map((order) => (
-              <TableRow key={order.id}>
-                <TableCell className="px-5 py-4 sm:px-6 text-start">
-                  <div className="flex items-center gap-3">
-                    <div className="w-10 h-10 overflow-hidden rounded-full">
-                      <img
-                        width={40}
-                        height={40}
-                        src={order.user.image}
-                        alt={order.user.name}
-                      />
-                    </div>
-                    <div>
-                      <span className="block font-medium text-gray-800 text-theme-sm dark:text-white/90">
-                        {order.user.name}
-                      </span>
-                      <span className="block text-gray-500 text-theme-xs dark:text-gray-400">
-                        {order.user.role}
-                      </span>
-                    </div>
-                  </div>
-                </TableCell>
-                <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                  {order.projectName}
-                </TableCell>
-                <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                  <div className="flex -space-x-2">
-                    {order.team.images.map((teamImage, index) => (
-                      <div
-                        key={index}
-                        className="w-6 h-6 overflow-hidden border-2 border-white rounded-full dark:border-gray-900"
-                      >
-                        <img
-                          width={24}
-                          height={24}
-                          src={teamImage}
-                          alt={`Team member ${index + 1}`}
-                          className="w-full size-6"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                </TableCell>
-                <TableCell className="px-4 py-3 text-gray-500 text-start text-theme-sm dark:text-gray-400">
-                  <Badge
-                    size="sm"
-                    color={
-                      order.status === "Active"
-                        ? "success"
-                        : order.status === "Pending"
-                        ? "warning"
-                        : "error"
-                    }
-                  >
-                    {order.status}
-                  </Badge>
-                </TableCell>
-                <TableCell className="px-4 py-3 text-gray-500 text-theme-sm dark:text-gray-400">
-                  {order.budget}
-                </TableCell>
-              </TableRow>
-            ))} */}
           </TableBody>
         </Table>
       </div>
